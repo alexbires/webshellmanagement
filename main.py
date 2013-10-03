61cab1a4f706b693b2fe40b107dd8a4baebc1e87
comp <alexbires@gmail.com>
2013-10-03 01:05:38 -0400

    Started work on the banner and accepting user input

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-import curses
+import sqlite3
 
-stdscr = curses.initscr()
+conn = sqlite3.connect(":memory:")
+cursor = conn.cursor()#setting up a memory database at the moment
+cursor.execute("create table vulnerable_sites(url text, page text, method text, uploaded_shell text)")
+
+toInsert = ['www.bing.com','about.php','get', '<?php passthru($_GET[\'cmd\']']
+cursor.execute("insert into vulnerable_sites values(?,?,?,?)",toInsert)
+conn.commit()
+
+
+def print_banner():
+	print "        _       __        __      _    __     __  __ _"
+	print "       / \\      \\ \\      / /     |"
+
+def get_input():
+	userinput = raw_input("awsmi>")
+
+print_banner()
